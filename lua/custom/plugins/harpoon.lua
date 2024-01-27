5147486c1592e2fd0a58e7181ab280449166a640
Miranda Schritt <miranda.schritt@gmail.com>
2024-01-27 05:54:08 -0500

    Adds Harpoon2
    Unable to use ^<C-S-P^>, ^<C-S-N^>, ^<C-[1-4]^> due to Windows terminal keybinding conflicts.

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,41 @@
+return {
+  'ThePrimeagen/harpoon',
+  lazy = true,
+  branch = 'harpoon2',
+  init = function()
+    local harpoon = require 'harpoon'
+
+    -- REQUIRED
+    harpoon:setup()
+    -- REQUIRED
+
+    vim.keymap.set('n', '<leader>m', function()
+      harpoon:list():append()
+    end)
+    vim.keymap.set('n', '<C-m>', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+
+    vim.keymap.set('n', '<A-1>', function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set('n', '<A-2>', function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set('n', '<A-3>', function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set('n', '<A-4>', function()
+      harpoon:list():select(4)
+    end)
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set('n', '<Tab>', function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set('n', '<S-Tab>', function()
+      harpoon:list():next()
+    end)
+  end,
+  dependencies = { 'nvim-lua/plenary.nvim' },
+}
