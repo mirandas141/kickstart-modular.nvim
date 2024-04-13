62382c6f734e12ae4d12ab8c42590604269dccdc
Miranda Schritt <mirandas141@gmail.com>
2024-04-13 16:54:54 -0400

    Adds support for Tmux panel integration
    Remaps panel navigation keys to C-(hjkl) to seemlessly navigate between NVim and Tmux panels

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -7,8 +7,4 @@ return {
     { desc = "Toggle horizonta term" }),
   vim.keymap.set({ 'n', 't' }, '<A-v>', function() require("nvterm.terminal").toggle "vertical" end,
     { desc = "Toggle vertica term" }),
-  vim.keymap.set('n', '<C-LEFT>', "<C-w>h", { desc = "Window left" }),
-  vim.keymap.set('n', '<C-DOWN>', "<C-w>j", { desc = "Window down" }),
-  vim.keymap.set('n', '<C-UP>', "<C-w>k", { desc = "Window up" }),
-  vim.keymap.set('n', '<C-RIGHT>', "<C-w>l", { desc = "Window right" }),
 }
