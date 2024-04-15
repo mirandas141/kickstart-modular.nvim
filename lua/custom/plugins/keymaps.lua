a7972bed983eb9c029e323d7f70421a0732d8012
Miranda Schritt <mirandas141@gmail.com>
2024-04-15 08:41:42 -0400

    Remove nvterm
    Removes nvterm as we're now using tmux for multiplexing so having a terminal inside of nvim is no longer needed

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,10 +1,4 @@
 return {
   vim.keymap.set({ 'n', 'v' }, ';', ":"),
   vim.keymap.set('i', 'jj', "<esc>", { desc = "Escape from insert mode" }),
-  vim.keymap.set({ 'n', 't' }, '<A-i>', function() require("nvterm.terminal").toggle "float" end,
-    { desc = "Toggle floating term" }),
-  vim.keymap.set({ 'n', 't' }, '<A-h>', function() require("nvterm.terminal").toggle "horizontal" end,
-    { desc = "Toggle horizonta term" }),
-  vim.keymap.set({ 'n', 't' }, '<A-v>', function() require("nvterm.terminal").toggle "vertical" end,
-    { desc = "Toggle vertica term" }),
 }
