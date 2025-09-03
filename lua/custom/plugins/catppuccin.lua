cf4b0f51c121c93c478e146123e7ca9d98a328ee
Miranda Schritt <miranda.schritt@gmail.com>
2025-09-03 12:22:53 -0400

    Sets catpuccin as the default used theme

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('catppuccin').setup {
+      auto_integrations = true,
+    }
+    vim.cmd.colorscheme 'catppuccin'
+  end,
+}
